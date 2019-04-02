58febc2c800d08ea57667295319ea3ff93a2c5c5
Joey Saha <joeysaha@hotmail.com>
2019-04-02 17:21:53 -0400

    tweaks

--- src/components/Contexts/test.js
+++ src/components/Contexts/test.js
@@ -1,13 +1,7 @@
 import React from 'react'
 
 const test = React.createContext({
-<<<<<<< HEAD
     value: 'nuttin'
-=======
-    value: {
-        email: 'nuttin dude'
-    }
->>>>>>> d3cc05a94bee726ad34a700feabceb42336dbc8d
 })
 
 export default test
